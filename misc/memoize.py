89d91c0400733fd055d6500eb1f205d5c42de0a6
Claus Martinsen <clauswrm@gmail.com>
2017-11-25 20:47:40 +0100

    Renamed fibonacci

--- misc/memoize.py
+++ misc/memoize.py
@@ -5,7 +5,7 @@ __author__ = 'Claus Martinsen'
 def memoize(func):
     """ A decorator to add the dynamic programming principle memoization to a function. """
     memory = dict()
-
+    
     def memoize_func(arg):
         if arg in memory:
             return memory[arg]
@@ -13,23 +13,23 @@ def memoize(func):
             value = func(arg)
             memory[arg] = value
         return value
-
+    
     return memoize_func
 
 
 if __name__ == '__main__':
     # Only executed when this module is run directly
     # The following is an example of how to use the decorator
-
-    @memoize  # Try running fib(40) with/without this!
-    def fib(n):
+    
+    @memoize  # Try running fibonacci(40) with/without this!
+    def fibonacci(n):
         if n == 1:
             return 0
         elif n == 2:
             return 1
-        return fib(n - 1) + fib(n - 2)
-
-
-    print(fib(40))
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+    
+    print(fibonacci(40))
     # Without memoization -> Exponential running time
     # With memoization -> Linear running time
